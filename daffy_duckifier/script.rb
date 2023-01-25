d23290cf04c267d089a403ac99fb9f809b0914e0
Hayley Witherell <hayleywitherell@gmail.com>
2023-01-25 12:55:17 -0700

    Add extra

--- daffy_duckifier/script.rb
+++ daffy_duckifier/script.rb
@@ -1,12 +1,15 @@
-print "Thtring, pleathe!: "
+print "What would you like to translate? "
 
 user_input = gets.chomp
 user_input.downcase!
 
 if user_input.include? "s"
   user_input.gsub!(/s/, "th")
+  puts "Translation: #{user_input}"
 else
-  puts "Nothing to do here!"
+  if user_input == ""
+    puts "Say something!"
+  else
+    puts "No translation necessary."
+  end
 end
-  
-puts "Your string is: #{user_input}"
