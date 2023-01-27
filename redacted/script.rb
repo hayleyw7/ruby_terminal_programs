3a177de4232133ea48fe9b1216839386084bef47
Hayley Witherell <hayleywitherell@gmail.com>
2023-01-27 14:49:07 -0700

    Add redacted

---
+++ redacted/script.rb
@@ -0,0 +1,15 @@
+puts "Type a phrase: "
+text = gets.chomp
+
+puts "Word to redact from phrase: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word != redact
+    print word + " "
+  else
+    print "REDACTED "
+  end
+end
